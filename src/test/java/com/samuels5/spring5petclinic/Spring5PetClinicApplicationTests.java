098f1f50aa82ab8fdaf7a541f04692b0906346e2
Sampuccino <Sampuccino@gmail.com>
2020-03-17 00:40:53 -0700

    Initial commit.

---
+++ src/test/java/com/samuels5/spring5petclinic/Spring5PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuels5.spring5petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
