2e4baf21dda37dcd93ef6c9a8126525e6c7e5375
Sampuccino <Sampuccino@gmail.com>
2020-03-18 21:53:41 -0700

    Adding POJOs. Closes #2

---
+++ src/main/java/com/samuels5/spring5petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.samuels5.spring5petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/samuels5/spring5petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.samuels5.spring5petclinic.model;
+
+public class Vet extends Person {
+}
